5da49eed06b4a444278cb410d346afb22ae5d977
Veronika T <veronikat@gmail.com>
2018-02-04 11:30:08 -0700

    Initial commit

---
+++ server.js
@@ -0,0 +1,73 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+
+app.set('view engine', 'hbs');
+
+app.use((request, response, next)=>{
+    var now = new Date().toString();
+    var printString = `${now}: ${request.method} ${request.url}`;
+    console.log(printString);
+    fs.appendFile('server.log', printString + '\n', (err) => {
+        if(err){
+            console.log('Unable to write to server.log')
+        }
+    });
+    next();
+});
+// app.use((request, response)=>{
+//     response.render('maintenance.hbs', {
+//         pageTitle: 'Maintenance Page',
+//         welcomeMessage: 'Maintenance and such'
+//     });
+// });
+app.use(express.static(__dirname + "/public"));
+hbs.registerHelper('getCurrentYear', ()=>{
+    //return 'test';
+    return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text)=>{
+    return text.toUpperCase();
+});
+
+app.get('/', (req, res)=>{
+    //res.send('<h1>Hello Express</h1>');
+    // res.send({
+    //     name: "Vern",
+    //     likes: [
+    //         "Reality TV",
+    //         "cats"
+    //     ]
+    // });
+    
+    res.render('welcome.hbs', {
+        pageTitle: 'Welcome Page',
+        //currentYear: new Date().getFullYear(),
+        welcomeMessage: 'Welcome and such'
+    });
+});
+
+app.get('/about', (req, res)=>{
+    //res.send('About page');
+    res.render('about.hbs', {
+        pageTitle: 'About Page',
+        //currentYear: new Date().getFullYear(),
+        //welcomeMessage: 'Welcome and such'
+    });
+});
+
+
+app.get('/bad', (req, res)=>{
+    res.send({
+        errorMessage: "BAD"
+    });
+});
+
+app.listen(3000, ()=>{
+    console.log("Server running on port 3000")
+});
